474b76e9bee5d878bc7d611bdb22c7a0816df031
Josip Maras <jomaras@gmail.com>
2012-05-04 15:09:18 +0200

    Added more tests, generating models for script elements..

---
+++ debug/testFiles/04/externalScript1.js
@@ -0,0 +1,7 @@
+/**
+ * User: Jomaras
+ * Date: 04.05.12.
+ * Time: 14:44
+ */
+
+var externalVariable1 = 2;
---
+++ debug/testFiles/04/externalScript2.js
@@ -0,0 +1,6 @@
+/**
+ * User: Jomaras
+ * Date: 04.05.12.
+ * Time: 14:44
+ */
+var externalVariable2 = 2;
