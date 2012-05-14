ee03850c952ee9d18be4536432d60f76e2e6117a
Josip Maras <jomaras@gmail.com>
2012-05-14 09:43:54 +0200

    Added more tests; expanded getting base objects for call expressions

--- chrome/content/Firecrow/interpreter/simulator/ExecutionContextStack.js
+++ chrome/content/Firecrow/interpreter/simulator/ExecutionContextStack.js
@@ -442,6 +442,7 @@ Firecrow.Interpreter.Simulator.ExecutionContextStack.prototype =
         if(ASTHelper.isIdentifier(codeConstruct)
         || ASTHelper.isFunctionExpression(codeConstruct)) { return this.globalObject; }
         else if (ASTHelper.isMemberExpression(codeConstruct)) { return this.getExpressionValue(codeConstruct.object); }
+        else if (ASTHelper.isCallExpression(codeConstruct)) { return this.getExpressionValue(codeConstruct.callee); }
         else { alert("ExecutionContextStack - not handling getting base object on other expressions"); return this.globalObject; }
     },
 
