efa05029429516fbc238dcdb9f7f7ceb17bc152a
Josip Maras <jomaras@gmail.com>
2012-05-25 08:26:57 +0200

    Backup before introducing JsValue all over the place

---
+++ chrome/content/Firecrow/interpreter/model/JsValue.js
@@ -0,0 +1,25 @@
+/**
+ * User: Jomaras
+ * Date: 25.05.12.
+ * Time: 08:15
+ */
+FBL.ns(function() { with (FBL) {
+    /*************************************************************************************/
+    const fcModel = Firecrow.Interpreter.Model;
+    const ValueTypeHelper = Firecrow.ValueTypeHelper;
+
+    fcModel.JsValue = function(value, fcInternal)
+    {
+        try
+        {
+            this.value = value;
+            this.fcInternal = fcInternal;
+        }
+        catch(e) { alert("JsValue - error when creating: " + e); }
+    };
+
+    fcModel.JsValue.prototype =
+    {
+
+    };
+}});
