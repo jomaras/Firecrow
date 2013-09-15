0969118897958a0a24a03937f7f84b21739c77b2
Josip Maras <jomaras@gmail.com>
2013-09-15 14:56:26 +0200

    Update to jQuery tests

--- phantomJs/evaluationHelpers/slicer.js
+++ phantomJs/evaluationHelpers/slicer.js
@@ -25,7 +25,7 @@ modelFiles = fs.list(rootFolder).map(function(fileName)
 {
     var fullPath = rootFolder + fs.separator + fileName;
 
-    if(fs.isFile(fullPath) && fullPath.indexOf('.json') != -1 && fullPath.indexOf("data08") != -1)
+    if(fs.isFile(fullPath) && fullPath.indexOf('.json') != -1 && fullPath.indexOf("effects03") != -1)
     {
         return fullPath;
     }
