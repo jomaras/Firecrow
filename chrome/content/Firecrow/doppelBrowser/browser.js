dd0a1afe0ff9ed8ef18a462fb4b1b9c1d6e4bc71
jomaras <rdaatR4>
2013-12-29 20:37:01 +0100

    When creating dependencies between css and html nodes, the matchesSelector function now ignores :hover, :active

--- chrome/content/Firecrow/doppelBrowser/browser.js
+++ chrome/content/Firecrow/doppelBrowser/browser.js
@@ -704,12 +704,12 @@ FBL.ns(function() { with (FBL) {
         matchesSelector: function(htmlElement, selector)
         {
             if(htmlElement == null || this._matchesSelector == null
-                || ValueTypeHelper.isDocumentFragment(htmlElement) || ValueTypeHelper.isTextNode(htmlElement)
-                || ValueTypeHelper.isDocument(htmlElement)) { return false; }
+            || ValueTypeHelper.isDocumentFragment(htmlElement) || ValueTypeHelper.isTextNode(htmlElement)
+            || ValueTypeHelper.isDocument(htmlElement)) { return false; }
 
             try
             {
-                return this._matchesSelector.call(htmlElement, selector);
+                return this._matchesSelector.call(htmlElement, selector) || this._matchesSelector.call(htmlElement, selector.replace(/:hover|:active/gi, ""));
             }
             catch(e)
             {
