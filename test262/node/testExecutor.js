8050ff6ad010e300b7c01b38ae2752794281ec1d
jomaras <rdaatR4>
2014-06-30 12:40:07 +0200

    Implementing test262 testing framework

--- test262/node/testExecutor.js
+++ test262/node/testExecutor.js
@@ -4,7 +4,14 @@
 var fs = require('fs');
 var fileHelper = require("./utils/FileHelper.js");
 
-var allTestFiles = fileHelper.getAllJavaScriptFilesFromSubfolders("C:\\GitWebStorm\\Firecrow\\test262\\test\\suite\\ch07");
+var allTestFiles = fileHelper.getAllJavaScriptFilesFromSubfolders("C:\\GitWebStorm\\Firecrow\\test262\\test\\suite\\ch06");
 
-//console.log();
-console.log(allTestFiles);
+for(var i = 0; i < allTestFiles.length; i++)
+{
+    var fileContent = fileHelper.getFileContent(allTestFiles[i]);
+    console.log("Executing test: " + allTestFiles[i]);
+    //Execute test
+
+    //Notify on results of a failing test
+    console.log("Executing test: " + allTestFiles[i]);
+}
